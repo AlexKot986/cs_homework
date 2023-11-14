464305ba32c73034054892eccaebde7eb3f8c9aa
Alexershov86 <alexershov86@mail.ru>
2023-11-14 23:16:03 +0300

    Task2

---
+++ Task_2.cs
@@ -0,0 +1,69 @@
+﻿// Урок 2. Условные операторы, циклы, массивы, строки, StringBuilder
+// Дан двумерный массив.
+
+// 732
+
+// 496
+
+// 185
+
+// Отсортировать данные в нем по возрастанию.
+
+// 123
+
+// 456
+
+// 789
+
+// Вывести результат на печать.
+
+// class Program
+
+// {
+
+// static void Main(string[] args)
+
+// {
+
+// int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+// }
+
+// }
+
+int[,] arr = {{7, 3, 2}, {4, 9, 6}, {1, 8, 5}};
+
+PrintSortArray(arr);
+
+static void PrintSortArray(int[,] arr)
+{
+    int[] a = new int[arr.Length];
+    int i = 0;
+
+    foreach (int n in arr)
+    {
+        a[i++] = n;
+    }
+
+    Array.Sort(a);
+
+    i = 0;
+
+    foreach (int n in a)
+    { 
+        if (i == arr.GetLength(0)) 
+        {
+            i = 0;
+            Console.WriteLine();
+        }
+        Console.Write(n);
+        i++;
+    }
+}
+
+
+
+
+
+
+
